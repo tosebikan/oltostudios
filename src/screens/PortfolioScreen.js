37e6ec0233f4f07bb31589cfb2f809bb2db71bc2
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-09-07 13:55:03 +0000

    add portfolio screen

---
+++ src/screens/PortfolioScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PortfolioScreen() {
+  return (
+    <div className="about-hero">
+      <h1> Portfolio Screen</h1>
+    </div>
+  );
+}
+
+export default PortfolioScreen;
