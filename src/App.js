81952c9ac5c4bc014d96815bf76312e39c739add
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-09-07 09:50:29 +0000

    add header section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun } from "@fortawesome/free-solid-svg-icons";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="brand">
+          <a href="/">oltostudios</a>
+        </div>
+        <div className="header-links">
+          <FontAwesomeIcon icon={faSun} className="header-icon" />
+          <button id="header-btn" className="header-button">
+            &#9776;
+          </button>
+        </div>
       </header>
     </div>
   );
