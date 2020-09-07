8173b441b2995bc9888b8b538b0a317c5e2177eb
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-09-07 13:55:54 +0000

    add contact screen

---
+++ src/screens/ContactScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ContactScreen() {
+  return (
+    <div className="about-hero">
+      <h1> Contact Screen</h1>
+    </div>
+  );
+}
+
+export default ContactScreen;
