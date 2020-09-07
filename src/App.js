6b22e3083ef9671f62a647ef4e76380cd2e469f7
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-09-07 13:56:43 +0000

    add sidebar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSun } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSun,
+  faAddressCard,
+  faAddressBook,
+  faBriefcase,
+  faTimes,
+  faBars
+} from "@fortawesome/free-solid-svg-icons";
 import "./App.css";
+import AboutScreen from "./screens/AboutScreen";
+import PortfolioScreen from "./screens/PortfolioScreen";
+import ContactScreen from "./screens/ContactScreen";
 
 function App() {
+  const [click, setClick] = useState(false);
+  const toggle = () => {
+    setClick(!click);
+    document.querySelector(".sidebar").classList.toggle("open");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="brand">
-          <a href="/">oltostudios</a>
-        </div>
-        <div className="header-links">
-          <FontAwesomeIcon icon={faSun} className="header-icon" />
-          <button id="header-btn" className="header-button">
-            &#9776;
-          </button>
-        </div>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <div className="brand">
+            <Link to="/">oltostudios</Link>
+          </div>
+          <div className="header-links">
+            <FontAwesomeIcon icon={faSun} className="header-icon" />
+            <button id="header-btn" className="header-button" onClick={toggle}>
+              {click ? (
+                <FontAwesomeIcon icon={faTimes} style={{ color: "#fff" }} />
+              ) : (
+                <FontAwesomeIcon icon={faBars} style={{ color: "#000" }} />
+              )}
+            </button>
+          </div>
+        </header>
+        <aside className="sidebar">
+          <ul>
+            <li>
+              <Link to="/" onClick={toggle}>
+                <FontAwesomeIcon icon={faAddressCard} className="link-icon" />
+                about
+              </Link>
+            </li>
+            <li>
+              <Link to="/portfolio" onClick={toggle}>
+                <FontAwesomeIcon icon={faBriefcase} className="link-icon" />
+                portfolio
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" onClick={toggle}>
+                <FontAwesomeIcon icon={faAddressBook} className="link-icon" />
+                contact
+              </Link>
+            </li>
+          </ul>
+        </aside>
+
+        <main>
+          <div>
+            <Route path="/" component={AboutScreen} exact={true} />
+            <Route path="/portfolio" component={PortfolioScreen} />
+            <Route path="/contact" component={ContactScreen} exact={true} />
+          </div>
+        </main>
+      </div>
+    </Router>
   );
 }
 
