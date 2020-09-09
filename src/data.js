0ef40851bf60e2cffbc236f22c57645aede1421a
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-09-09 13:57:24 +0000

    add portfolio data

---
+++ src/data.js
@@ -0,0 +1,55 @@
+const data = {
+  webTech: [
+    { tech: "Show-All" },
+    { tech: "React.js" },
+    { tech: "Front-End" },
+    { tech: "Full-Stack" },
+    { tech: "Back-End" },
+    { tech: "Redux" },
+    { tech: "Mongodb" },
+    { tech: "HTML5" },
+    { tech: "Bootstrap" },
+    { tech: "UI/UX" },
+    { tech: "Firebase" }
+  ],
+  webProjects: [
+    {
+      id: "1",
+      title: "Richhomes",
+      images: [require("./images/webimg.png"), require("./images/web2img.png")],
+      tech: ["React", "Front-End", "UI/UX"],
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis.",
+      url: "https://richhomesgh.com"
+    },
+    {
+      id: "2",
+      title: "Axtella",
+      images: [require("./images/webimg.png"), require("./images/web2img.png")],
+      tech: ["Gatsby", "Front-End", "UI/UX"],
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis.",
+      url: "https://richhomesgh.com"
+    },
+    {
+      id: "3",
+      title: "Carparts",
+      images: [require("./images/webimg.png"), require("./images/web2img.png")],
+      tech: ["React", "Redux", "MongoDB", "Full-Stack", "Front-End", "UI/UX"],
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis.",
+      url: "https://richhomesgh.com"
+    },
+    {
+      id: "4",
+      title: "Todos",
+      images: [require("./images/webimg.png"), require("./images/web2img.png")],
+      tech: ["React", "Full-Stack", "Front-End", "UI/UX"],
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis.",
+      url: "https://richhomesgh.com"
+    }
+  ]
+};
+
+export default data;
