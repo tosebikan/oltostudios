130570da00a306bf28a9505fdd9e702a800ff723
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-09-10 12:39:06 +0000

    add mobile app data

--- src/data.js
+++ src/data.js
@@ -12,6 +12,17 @@ const data = {
     { tech: "UI/UX" },
     { tech: "Firebase" }
   ],
+  mobileTech: [
+    { tech: "Show-All" },
+    { tech: "React-Native" },
+    { tech: "Firebase" },
+    { tech: "Front-End" },
+    { tech: "Full-Stack" },
+    { tech: "Back-End" },
+    { tech: "Redux" },
+    { tech: "Mongodb" },
+    { tech: "UI/UX" }
+  ],
   webProjects: [
     {
       id: "1",
@@ -35,7 +46,15 @@ const data = {
       id: "3",
       title: "Carparts",
       images: [require("./images/webimg.png"), require("./images/web2img.png")],
-      tech: ["React", "Redux", "MongoDB", "Full-Stack", "Front-End", "UI/UX"],
+      tech: [
+        "React",
+        "Redux",
+        "MongoDB",
+        "Full-Stack",
+        "Front-End",
+        "UI/UX",
+        "Back-End"
+      ],
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis.",
       url: "https://richhomesgh.com"
@@ -44,10 +63,102 @@ const data = {
       id: "4",
       title: "Todos",
       images: [require("./images/webimg.png"), require("./images/web2img.png")],
-      tech: ["React", "Full-Stack", "Front-End", "UI/UX"],
+      tech: [
+        "React",
+        "Full-Stack",
+        "Front-End",
+        "UI/UX",
+        "MongoDB",
+        "Back-End"
+      ],
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis.",
       url: "https://richhomesgh.com"
+    },
+    {
+      id: "5",
+      title: "Pharma",
+      images: [require("./images/webimg.png"), require("./images/web2img.png")],
+      tech: [
+        "React",
+        "Full-Stack",
+        "Front-End",
+        "UI/UX",
+        "Firebase",
+        "Back-End"
+      ],
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis.",
+      url: "https://pharma.com"
+    }
+  ],
+  mobileProjects: [
+    {
+      id: "1",
+      title: "Spineup",
+      images: [require("./images/webimg.png"), require("./images/web2img.png")],
+      tech: ["React", "Front-End", "UI/UX"],
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis.",
+      url: "https://richhomesgh.com"
+    },
+    {
+      id: "2",
+      title: "Enroute",
+      images: [require("./images/webimg.png"), require("./images/web2img.png")],
+      tech: ["Gatsby", "Front-End", "UI/UX"],
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis.",
+      url: "https://richhomesgh.com"
+    },
+    {
+      id: "3",
+      title: "Newsify",
+      images: [require("./images/webimg.png"), require("./images/web2img.png")],
+      tech: [
+        "React",
+        "Redux",
+        "MongoDB",
+        "Full-Stack",
+        "Front-End",
+        "UI/UX",
+        "Back-End"
+      ],
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis.",
+      url: "https://richhomesgh.com"
+    },
+    {
+      id: "4",
+      title: "Movees",
+      images: [require("./images/webimg.png"), require("./images/web2img.png")],
+      tech: [
+        "React",
+        "Full-Stack",
+        "Front-End",
+        "UI/UX",
+        "MongoDB",
+        "Back-End"
+      ],
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis.",
+      url: "https://richhomesgh.com"
+    },
+    {
+      id: "5",
+      title: "Chatter",
+      images: [require("./images/webimg.png"), require("./images/web2img.png")],
+      tech: [
+        "React",
+        "Full-Stack",
+        "Front-End",
+        "UI/UX",
+        "Firebase",
+        "Back-End"
+      ],
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis.",
+      url: "https://pharma.com"
     }
   ]
 };
