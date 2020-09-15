4830528fba201963cbe18ff697b52f26c3b61b94
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-09-15 19:09:14 +0000

    add portfolio details resources

--- src/screens/PortfolioDetailsScreen.js
+++ src/screens/PortfolioDetailsScreen.js
@@ -1,16 +1,21 @@
 import React from "react";
 import "./PortfolioDetails.css";
 import data from "../data";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import {
+  faArrowUp,
+  faExternalLinkAlt,
+  faLink
+} from "@fortawesome/free-solid-svg-icons";
 
 function PortfolioDetailsScreen(props) {
-  console.log(props.match.params.id);
-
   const project = data.projects.find((x) => x.id === props.match.params.id);
 
   return (
     <div className="portfolio-details__hero">
       <h4>{project.title}</h4>
-      <p>SpineUp mobile React-Native application </p>
+      <p>{project.info}</p>
       <div className="portfolio-details__group">
         <div className="portfolio-details__image-container">
           <img
@@ -30,6 +35,35 @@ function PortfolioDetailsScreen(props) {
                 <li key={id}>{tech}</li>
               ))}
             </ul>
+          </div>
+          <div className="portfolio-details__resources">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={`${project.url}`}
+              className="project__link"
+            >
+              <button className="portfolio__github-button">
+                <FontAwesomeIcon
+                  icon={faExternalLinkAlt}
+                  className="github-icon"
+                />
+                visit link
+                <FontAwesomeIcon icon={faLink} className="link-icon" />
+              </button>
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={`${project.githubUrl}`}
+              className="project__link"
+            >
+              <button className="portfolio__website-button">
+                <FontAwesomeIcon icon={faGithub} className="github-icon" />
+                source code
+                <FontAwesomeIcon icon={faArrowUp} className="arrow-icon" />
+              </button>
+            </a>
           </div>
         </div>
       </div>
