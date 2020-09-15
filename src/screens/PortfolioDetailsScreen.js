75e63a23d2918b0f8823dcab842f9244c738c028
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-09-15 16:58:13 +0000

    add portfolio details screen

--- src/screens/PortfolioDetailsScreen.js
+++ src/screens/PortfolioDetailsScreen.js
@@ -1,9 +1,40 @@
 import React from "react";
+import "./PortfolioDetails.css";
+import data from "../data";
 
 function PortfolioDetailsScreen(props) {
-  const id = props.match.params.id;
-  console.log(id);
-  return <div>Portfolio Details</div>;
+  console.log(props.match.params.id);
+
+  const project = data.projects.find((x) => x.id === props.match.params.id);
+
+  return (
+    <div className="portfolio-details__hero">
+      <h4>{project.title}</h4>
+      <p>SpineUp mobile React-Native application </p>
+      <div className="portfolio-details__group">
+        <div className="portfolio-details__image-container">
+          <img
+            src={project.images[0]}
+            alt=""
+            className="portfolio-details__image"
+          />
+        </div>
+        <div className="portfolio-details__info">
+          <img src={project.logo} alt="" className="logo__image" />
+          <p>{project.description}</p>
+          <h4>technical sheet</h4>
+          <p>Technologies used to complete the project</p>
+          <div className="tech_sheet">
+            <ul className="property-details__list">
+              {project.tech.map((tech, id) => (
+                <li key={id}>{tech}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PortfolioDetailsScreen;
