bff3fb7930bb3e8be68f5f8387a451468d7b7336
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-09-15 10:10:11 +0000

    add link to card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,28 @@
 import React from "react";
 import "./card.css";
+import { Link } from "react-router-dom";
 
-function Card({ title, tech }) {
+function Card({ title, tech, link }) {
   return (
-    <div className="card-container">
-      <img
-        src={require("../images/webimg.png")}
-        alt=""
-        className="card-image"
-      />
-      <h1 className="card-image-text">{title}</h1>
-      <img
-        src={require("../images/web2img.png")}
-        alt=""
-        className="card-image2"
-      />
-      <div className="card-tech">
-        <p>{tech}</p>
-        <p>... see more</p>
+    <Link to={link}>
+      <div className="card-container">
+        <img
+          src={require("../images/webimg.png")}
+          alt=""
+          className="card-image"
+        />
+        <h1 className="card-image-text">{title}</h1>
+        <img
+          src={require("../images/web2img.png")}
+          alt=""
+          className="card-image2"
+        />
+        <div className="card-tech">
+          <p>{tech}</p>
+          <p>... see more</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
