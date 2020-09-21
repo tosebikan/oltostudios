4e678ef2671e1a86260d3bc49ecb0c23e28b7c13
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-09-21 11:58:30 +0000

    add props to card

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,21 +2,13 @@ import React from "react";
 import "./card.css";
 import { Link } from "react-router-dom";
 
-function Card({ title, tech, link }) {
+function Card({ title, tech, link, image1, image2 }) {
   return (
     <Link to={link}>
       <div className="card-container">
-        <img
-          src={require("../images/webimg.png")}
-          alt=""
-          className="card-image"
-        />
+        <img src={image1} alt="" className="card-image" />
         <h1 className="card-image-text">{title}</h1>
-        <img
-          src={require("../images/web2img.png")}
-          alt=""
-          className="card-image2"
-        />
+        <img src={image2} alt="" className="card-image2" />
         <div className="card-tech">
           <p>{tech}</p>
           <p>... see more</p>
